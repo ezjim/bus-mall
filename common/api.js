c7e5edece715ba825d17e31ac692e643870f63a7
James <james.re@yahoo.com>
2020-01-27 22:42:16 -0800

    api mathrndm findby

---
+++ common/api.js
@@ -0,0 +1,124 @@
+const products = [
+    {
+        id: 'bag',
+        image: './assets/bag.jpg',
+        name: 'Bag'
+    },
+
+    {
+        id: 'banana',
+        image: './assets/banana.jpg',
+        name: 'Banana'
+    },
+    
+    {
+        id: 'bathroom',
+        image: './assets/bathroom.jpg',
+        name: 'Bathroom'
+    },
+
+    {
+        id: 'boots',
+        image: './assets/boots.jpg',
+        name: 'Boots'
+    },
+
+    {
+        id: 'breakfast',
+        image: './assets/breakfast.jpg',
+        name: 'Breakfast'
+    },
+
+    {
+        id: 'bubblegum',
+        image: './assets/bubblegum.jpg',
+        name: 'Bubblegum'
+    },
+    
+    {
+        id: 'chair',
+        image: './assets/chair.jpg',
+        name: 'Chair'
+    },
+    
+    {
+        id: 'cthulhu',
+        image: './assets/ctulhu.jpg',
+        name: 'Ctulhu'
+    },
+
+    {
+        id: 'dog-duck',
+        image: './assets/dog-duck.jpg',
+        name: 'Dog-Duck'
+    },
+
+    {
+        id: 'dragon',
+        image: './assets/dragon.jpg',
+        name: 'Dragon'
+    },
+    
+    {
+        id: 'pen',
+        image: './assets/pen.jpg',
+        name: 'Pen'
+    },
+    
+    {
+        id: 'pet-sweep',
+        image: './assets/pet-sweep.jpg',
+        name: 'Pet-Sweep'
+    },
+
+    {
+        id: 'scissors',
+        image: './assets/scissors.jpg',
+        name: 'Scissors'
+    },
+
+    {
+        id: 'shark',
+        image: './assets/shark.jpg',
+        name: 'Shark'
+    },
+    
+    {
+        id: 'sweep',
+        image: './assets/sweep.jpg',
+        name: 'Sweep'
+    },
+    
+    {
+        id: 'tauntaun',
+        image: './assets/tauntaun.jpg',
+        name: 'Tauntaun'
+    },
+
+    {
+        id: 'unicorn',
+        image: './assets/unicorn.jpg',
+        name: 'Unicorn'
+    },
+
+    {
+        id: 'usb',
+        image: './assets/usb.jpg',
+        name: 'Usb'
+    },
+    
+    {
+        id: 'water-can',
+        image: './assets/water-can.jpg',
+        name: 'Water-Can'
+    },
+    
+    {
+        id: 'wine-glass',
+        image: './assets/wine-glass.jpg',
+        name: 'Wine-Glass'
+    },
+];
+
+
+export default products;
---
+++ common/utils.js
@@ -0,0 +1,14 @@
+// Get Random Int Function
+export function getRandomInt(lenth) {
+    return Math.floor(Math.random() = length);
+}
+// a random int(and # value in the array)
+
+export function findbyProduct(product, id) => {
+
+    products.forEach((product, id) => {
+        console.log(product)
+    }
+    
+});
+// doubble check and see if this is correct. 
